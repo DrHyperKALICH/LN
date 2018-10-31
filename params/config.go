f3b60828c47bd37bdfa346ce1abe539f1d856868
Sergey Kalich R3UAL <drhyperkalich@gmail.com>
2018-10-31 23:36:52 +0300

    TestnetGenesisHash

--- params/config.go
+++ params/config.go
@@ -26,7 +26,7 @@ import (
 // Genesis hashes to enforce below configs on.
 var (
 	MainnetGenesisHash = common.HexToHash("0xd4e56740f876aef8c010b86a40d5f56745a118d0906a34e69aec8c0db1cb8fa3")
-	TestnetGenesisHash = common.HexToHash("0x41941023680923e0fe4d74a34bdac8141f2540e3ae90623718e47d66d1ca4a2d")
+	TestnetGenesisHash = common.HexToHash("0x16b64bc28351c3f3b6e8bbc320d67e0b0da8ad7887dd7bff237291f51305cbc7")
 	RinkebyGenesisHash = common.HexToHash("0x6341fd3daf94b748c72ced5a5b26028f2474f5f00d824504e4fa37a75767e177")
 )
 
